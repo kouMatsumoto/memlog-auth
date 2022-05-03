d6e930f201152e5712062cb7a708528eba480df8
Kou Matsumoto <develop@kou-matsumoto.jp>
2022-05-03 10:15:39 +0900

    feat: register github oauth config

--- main.ts
+++ main.ts
@@ -2,8 +2,13 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 console.log("Listening on http://localhost:8000");
 
+const githubOAuthAppConfig = {
+  clientId: Deno.env.get("CLIENT_ID"),
+  clientSecret: Deno.env.get("CLIENT_SECRET"),
+};
+
 serve((_req) => {
-  return new Response("Hello World!", {
+  return new Response("OK" + githubOAuthAppConfig.clientId, {
     headers: { "content-type": "text/plain" },
   });
 });
